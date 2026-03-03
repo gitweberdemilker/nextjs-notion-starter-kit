23a39c130d1aca0eb32ba06bf1d09e560c3fec6c
gitweberdemilker <erdemilker@hotmail.com>
2026-03-03 11:39:05 +0300

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '068ed52150234a9ea95768a33ee23fa1',
+  rootNotionPageId: '318ddc548f4180a3ac2fe2dea9adc63f',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
