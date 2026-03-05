e3b17962c0e62501e67c495255f6bea1510ccc15
gitweberdemilker <erdemilker@hotmail.com>
2026-03-05 17:49:20 +0300

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -50,10 +50,10 @@ export default siteConfig({
     '/kitaplar': '318ddc548f4180358a6fd72ac0e8b557',
     '/hikayeler': '318ddc548f41808f9deffcbf9d8292cc',
     '/seyahatname': '318ddc548f418083b264c9dfd0215b5b',
-    // === EKSİK OLAN 3 SAYFA BURAYA EKLENDİ ===
-    '/yeni-site-hakknda': '31addc548f41801582fbffab10ec3453',
-    '/karanlk-seyahatname': '31addc548f4180fbab4be8692f5f22ce',
-    '/cinayet-rota-oluturuldu-tantm': '31addc548f41802b84b9f52064e8a30a'
+    // === HARF DÜŞMELERİ GİDERİLMİŞ SON EKLENTİLER ===
+    '/yeni-site-hakkinda': '31addc548f41801582fbffab10ec3453',
+    '/karanlik-seyahatname': '31addc548f4180fbab4be8692f5f22ce',
+    '/cinayet-rota-olusturuldu-tanitim': '31addc548f41802b84b9f52064e8a30a'
   },
 
   navigationStyle: 'custom',
